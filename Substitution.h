a04213bd9c05f1dff9fa2dd797db4b5355f3f011
Sergey Makarov <sergey_makarov_official@mail.ru>
2017-07-09 01:09:47 +0300

    Some changes

--- Substitution.h
+++ Substitution.h
@@ -4,29 +4,29 @@ using namespace std;
 class Substitution
 {
 private:
-	string ShiftUppercase; // ������� ��������
-	bool IsEmpty(string s); // ����� �������� �� ������ ��������� ������
-	inline char ConversionSymbol(char& i); // ����� ������ ������ �������
+	string ShiftUppercase;
+	bool IsEmpty(string s);
+	inline char ConversionSymbol(char& i);
 	inline char DisConversionSymbol(char& i);
 	void TranslationTable();
 	void SetTable();
 public:
 	void Encription(string s);
     void DisEncription(string s);
-	Substitution(string table) :ShiftUppercase(table) {}; // �����������, ����������� �� ���� ������ � �����
+	Substitution(string table) :ShiftUppercase(table) {};�
 };
 
 class Substitution_shift
 {
 private:
-	int shift; // ���������� ��� �����
-	bool IsEmpty(string s); // ����� �������� �� ������ ��������� ������
-	inline char ConversionSymbol(char& i); // ����� ������ ������ �������
-	inline char DisConversionSymbol(char& i); // ����� �������� ������ ������ �������
+	int shift;
+	bool IsEmpty(string s);
+	inline char ConversionSymbol(char& i);
+	inline char DisConversionSymbol(char& i);
 public:
     void Encription(string s);
     void DisEncription(string s);
-	Substitution_shift(int n) :shift(n){}; // �����������, ����������� �� ���� �����
+	Substitution_shift(int n) :shift(n){};
 };
 
 
